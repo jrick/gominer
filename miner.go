01dfbf52ec7dd3864382fadf2a896ba1f868d6b2
John C. Vernaleo <john@netpurgatory.com>
2016-08-16 11:17:26 -0400

    Get gpus

--- miner.go
+++ miner.go
@@ -9,6 +9,8 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/mumax/3/cuda/cu"
+
 	"github.com/decred/gominer/cl"
 	"github.com/decred/gominer/stratum"
 	"github.com/decred/gominer/work"
@@ -45,6 +47,9 @@ func NewMiner() (*Miner, error) {
 	}
 
 	if cfg.UseCuda {
+		cu.Init(0)
+		ids := cu.DeviceGetCount()
+		minrLog.Infof("%v GPUs", ids)
 	} else {
 		platformIDs, err := getCLPlatforms()
 		if err != nil {
