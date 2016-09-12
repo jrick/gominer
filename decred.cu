252746f4eac20abf63acab6236b2ef93be148c09
Josh Rickmar <jrick@companyzero.com>
2016-09-12 13:55:49 +0000

    lalala

--- decred.cu
+++ decred.cu
@@ -184,7 +184,13 @@ __global__ void decred_gpu_hash_nonce(const uint32_t threads, const uint32_t sta
 		}
 	}
 }
+}
 
+extern "C" {
+void decred_hash_nonce(uint32_t grid, uint32_t block, uint32_t threads, uint32_t startNonce, uint32_t *resNonce, uint32_t targetHigh)
+{
+	decred_gpu_hash_nonce <<<grid, block>>> (threads, startNonce, resNonce, targetHigh);
+}
 }
 
 extern "C" {
